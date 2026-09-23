45541b9f26f750f6d681f80498551348d1710d07
Alireza Vafaei Sadr <vafaei.sadr@gmail.com>
2026-09-22 23:14:17 -0400

    Validate RRT endpoint refinement

--- scripts/validate_manuscript_robustness.sh
+++ scripts/validate_manuscript_robustness.sh
@@ -12,4 +12,5 @@ cd "$(git rev-parse --show-toplevel)"
   src/49_evaluate_documentation_context_sensitivity.py \
   src/50_evaluate_semantic_scores_only.py \
   src/51_audit_multitask_endpoints.py \
-  src/52_audit_strict_multitask_endpoints.py
+  src/52_audit_strict_multitask_endpoints.py \
+  src/53_refine_rrt_endpoint.py
